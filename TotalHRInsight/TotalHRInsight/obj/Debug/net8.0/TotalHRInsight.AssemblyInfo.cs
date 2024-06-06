ebd4af13f88c53182e133c6672cf1938fca2db4a
Melanie Picado <mpicado30750@ufide.ac.cr>
2024-06-05 20:38:06 -0600

    Estilos

--- TotalHRInsight/TotalHRInsight/obj/Debug/net8.0/TotalHRInsight.AssemblyInfo.cs
+++ TotalHRInsight/TotalHRInsight/obj/Debug/net8.0/TotalHRInsight.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotalHRInsight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6538d16474d64caffad17ccfc2ef224f010723d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97959ceafa158bac2f1bec9719f03d7def904ba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotalHRInsight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotalHRInsight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
