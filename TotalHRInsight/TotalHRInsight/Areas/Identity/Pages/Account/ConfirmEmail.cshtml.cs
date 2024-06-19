3c924f0f1b03828f287987d142465cd5445053c3
Scruz070 <147681327+Scruz070@users.noreply.github.com>
2024-06-18 22:38:18 -0600

    Cambios Steven

--- TotalHRInsight/TotalHRInsight/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ TotalHRInsight/TotalHRInsight/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -45,7 +45,7 @@ namespace TotalHRInsight.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            StatusMessage = result.Succeeded ? "Gracias por confirmar su correo electrónico." : "Error al confirmar su correo electrónico.";
             return Page();
         }
     }
