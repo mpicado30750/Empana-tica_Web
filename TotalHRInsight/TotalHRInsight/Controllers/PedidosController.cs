b7c17eacd0d24ddc5737e42010e2a0b0ecdfbab9
Scruz070 <147681327+Scruz070@users.noreply.github.com>
2024-07-09 21:13:15 -0600

    Steven

--- TotalHRInsight/TotalHRInsight/Controllers/PedidosController.cs
+++ TotalHRInsight/TotalHRInsight/Controllers/PedidosController.cs
@@ -375,8 +375,7 @@ namespace TotalHRInsight.Controllers
                     dataRow.Cell(5).Value = pedido.Sucursal.NombreSucursal;
                     dataRow.Cell(6).Value = pedido.Estado.EstadoSolicitud;
                     dataRow.Cell(7).Value = pedido.MontoTotal;
-                    dataRow.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
-                  //  dataRow.Style.Border.BottomBorderColor = XLColor.Black;
+                 
                     rowIdx++;
                 }
 
