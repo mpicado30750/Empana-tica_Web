32a779203721f3a46ad4cd46553b03c153571a45
Gretchen <gretks@hotmail.com>
2024-07-21 13:49:12 -0600

    UsuarioCambios

--- TotalHRInsight/TotalHRInsight/wwwroot/js/loader.js
+++ TotalHRInsight/TotalHRInsight/wwwroot/js/loader.js
@@ -1,4 +1,4 @@
-// wwwroot/js/loader.js
+ wwwroot/js/loader.js
 function showLoader() {
     setTimeout(function () {
         var loaderModal = new bootstrap.Modal(document.getElementById('loaderModal'));
